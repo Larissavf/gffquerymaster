4b11273b2b6ef0671a8d83c529ef06b9f10e6660
lvoshol <lvoshol@st.hanze.nl>
2024-10-15 17:22:04 +0200

    added the OutputWriter fixed all the errors and is now working

--- src/main/java/nl/bioinf/FileReader.java
+++ src/main/java/nl/bioinf/FileReader.java
@@ -1,11 +1,10 @@
 /**
- * This is a JavaDoc comment for a file
+ * to read a file line for line, filter the line, and write it to the output file
  *
- * This file contains the main application logic for our program.
  *
- * @author Your Name
+ * @author Cheyenne & Larissa
  * @version 1.0
- * @since 2023-02-20
+ * @since 5-10-2024
  */
 package nl.bioinf;
 
@@ -16,53 +15,68 @@ import org.apache.logging.log4j.Logger;
 import javax.sound.sampled.Line;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
-// Goes through all the lines makes feature object witch gets combined in a linked list
-// extra list with the heridatory of the objects
 public class FileReader {
+    private static final Logger logger = LogManager.getLogger(FileReader.class.getName());
+    private List<LineSeparator> gffFile = new ArrayList<>();
+    private List<String> startGffFile = new ArrayList<>();
 
     /**
      * Parses through the GFF file line by line and extracts information.
      *
-     * @param filePath The path to the GFF file to be parsed.
+     * @param inputFilePath The path to the GFF file to be parsed.
+     * @param outputFilePath the path to the gff file needs to be stored
      */
-    private static final Logger logger = LogManager.getLogger(FileReader.class.getName());
-    private List<LineSeparator> gffFile;
     //todo prio2 overload constructor with just the different combinations with the filtering
-    public List<LineSeparator> parseGFFFile(String filePath) {
-        try (BufferedReader br = new BufferedReader(new java.io.FileReader(filePath))) {
+    public void parseGFFFile(String inputFilePath, String outputFilePath) {
+        // create end write product
+        OutputWriter product = new OutputWriter(inputFilePath, outputFilePath);
+        product.createTheFile();
+
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(inputFilePath))) {
             String line;
 
             while ((line = br.readLine()) != null) {
                 // Skip comment lines and headers
                 if (line.startsWith("#")) {
+                    product.writeOutput(line);
                     continue;
-                    //todo prio1 onthoud voor output
                 }
 
                 // Split the line into columns
                 String[] columns = line.split("\t");
                 LineSeparator separatedRow = new LineSeparator(columns);
-
-                gffFile.add(separatedRow);
+                product.writeOutput(separatedRow);
             }
         } catch (IOException e) {
-            logger.fatal("An error occurred while parsing the GFF file: " + filePath, e);
+            logger.fatal("An error occurred while parsing the GFF file: " + inputFilePath, e);
         }
-        return gffFile;
     }
+    /**
+     * Parses through the GFF file line by line and extracts information.
+     * Filters the line based on the column name and what value it should be equal to.
+     *
+     * @param inputFilePath The path to the GFF file to be parsed.
+     * @param columnName the object with the column name and the value it's supposed to equal to
+     * @param outputFilePath The path the gff file will be written to
+     */
 
-    public List<LineSeparator> parseGFFFile(String filePath, String columnName) {
-        try (BufferedReader br = new BufferedReader(new java.io.FileReader(filePath))) {
+    public void parseGFFFile(String inputFilePath, String columnName, String outputFilePath) {
+        // create end write product
+        OutputWriter product = new OutputWriter(inputFilePath, outputFilePath);
+        product.createTheFile();
+
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(inputFilePath))) {
             String line;
 
             while ((line = br.readLine()) != null) {
                 // Skip comment lines and headers
                 if (line.startsWith("#")) {
+                    product.writeOutput(line);
                     continue;
-                    //todo prio1 onthoud voor output
                 }
 
                 // Split the line into columns
@@ -70,13 +84,13 @@ public class FileReader {
                 LineSeparator separatedRow = new LineSeparator(columns);
                 // if line is correct to the filtered value
                 if (Filter.ColumnName(separatedRow, columnName)){
-                    gffFile.add(separatedRow);
+                    product.writeOutput(separatedRow);
                 }
 
             }
+            logger.info("The file is read and filtered");
         } catch (IOException e) {
-            logger.fatal("An error occurred while parsing the GFF file: " + filePath, e);
+            logger.fatal("An error occurred while parsing the GFF file: " + inputFilePath, e);
         }
-        return gffFile;
     }
 }
--- src/main/java/nl/bioinf/Filter.java
+++ src/main/java/nl/bioinf/Filter.java
@@ -1,3 +1,11 @@
+/**
+ * For filtering the file, by checking the object holding the line if it contains the wanted value
+ *
+ *
+ * @author Cheyenne & Larissa
+ * @version 1.0
+ * @since 10-10-2024
+ */
 package nl.bioinf;
 
 import org.apache.logging.log4j.LogManager;
@@ -8,12 +16,37 @@ import java.util.Arrays;
 
 public class Filter {
 
-    private static final Logger logger = LogManager.getLogger(ArgsProcessor.class.getName());
+    private static final Logger logger = LogManager.getLogger(Filter.class.getName());
 
+    /**
+     * Checks if the line has the same value as been asked to filter on
+     *
+     * @param readLine the object made from the line from the file
+     * @param filterObject the String that has the filtercolumn and filtervalue seperated on a "="
+     *
+     * @return true if the line is equal to the asked value and needs to be used for the output
+     */
     public static boolean ColumnName(LineSeparator readLine, String filterObject) {
-        // Filter items
-        String filterColumn = filterObject.split(" ")[0];
-        String filterValue = filterObject.split(" ")[1];
+       String filterColumn= "";
+       String filterValue = "";
+
+        // extract filter items
+        try {
+            filterColumn = filterObject.split("=")[0];
+            filterValue = filterObject.split("=")[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            // Handle the case where "=" is missing or the format is invalid
+            logger.fatal("Invalid format: expected 'key=value', but got: " + filterObject);
+            System.exit(1);
+        } catch (NullPointerException e) {
+            // Handle the case where filterObject is null
+            logger.fatal("Error: The filterObject is null.");
+            System.exit(1);
+        } catch (Exception e) {
+            // Catch any other unexpected exceptions
+            logger.fatal("An unexpected error occurred: " + e.getMessage());
+            System.exit(1);
+        }
 
         // Possible filter values
         String[] possibleInput = {"sequenceID", "source", "featureType", "startAndStop"};
@@ -24,7 +57,7 @@ public class Filter {
                 throw new Exception("The given column name is different from the allowed options");
             }
         } catch (Exception e) {
-            logger.error((Marker) e, "The given column name is different from the options, change it to the allowed options");
+            logger.error("The given column name is different from the options, change it to the allowed options");
         }
 
         // Returns a boolean if the value of the line is the same as the filter value
@@ -36,7 +69,13 @@ public class Filter {
             default -> false;
         };
     }
-
+    /**
+     * Checks if the coordinate values have at least a single nucleotide in the wanted region
+     *
+     * @param readLine the object made from the line from the file
+     * @param filterValue the String that has the filtercolumn and filtervalue seperated on a "="
+     * @return true if the line has coordinates fitting in the asked region
+     */
     // Prio1: Filter step to determine which features are within the x to y coordinate range
     public static boolean coordinates(LineSeparator readLine, String filterValue) {
         // Assuming filterValue is in the format "start-end", e.g., "1000-2000"
